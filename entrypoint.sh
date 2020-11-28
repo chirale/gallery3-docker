6c0b461775f92726faa60d003e08dfbc1ad7a822
Bradley W Dutton <brad@duttonbros.com>
2020-11-27 19:36:51 -0800

    add PHP 7.4 opcache preload

--- entrypoint.sh
+++ entrypoint.sh
@@ -16,6 +16,16 @@ if [ ! -z "${SITE_PROTOCOL}" ]; then
   echo "env[SITE_PROTOCOL] = ${SITE_PROTOCOL}" >> /etc/php/7.4/fpm/pool.d/www.conf
 fi
 
+sed -i 's/^opcache.*//' /etc/php/7.4/fpm/php.ini
+
+echo "opcache.enable=1" >> /etc/php/7.4/fpm/php.ini
+# 18 covers stock gallery, bump up for extra themes/modules
+echo "opcache.memory_consumption=24" >> /etc/php/7.4/fpm/php.ini
+echo "opcache.validate_timestamps=0" >> /etc/php/7.4/fpm/php.ini
+echo "opcache.preload=/var/www/lib/preload_opcache.php" >> /etc/php/7.4/fpm/php.ini
+echo "opcache.preload_user=root" >> /etc/php/7.4/fpm/php.ini
+
+
 chown www-data:www-data /var/www/var
 
 /etc/init.d/nginx start
